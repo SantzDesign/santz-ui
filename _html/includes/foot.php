d911e3730d4cce5a7087051d7e26264f2975cd35
Santiago Baigorria <hi@santz.com.ar>
2014-09-05 11:46:06 -0300

    SEO, Normalize, more modular

--- _html/includes/foot.php
+++ _html/includes/foot.php
@@ -1,5 +1,5 @@
   <!-- Scripts -->
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
 
 </body>
 </html>
--- _html/includes/head.php
+++ _html/includes/head.php
@@ -1,34 +1,57 @@
 <!DOCTYPE HTML>
-<html lang="en">
+<html lang="en" prefix="og: http://ogp.me/ns#">
 <head>
   <!-- Metadata -->
     <meta charset="UTF-8">
+    <title>Website Title</title>
     <meta name="description" content="Website description.">
+    <meta name="keywords" content="some, keywords, describing, site, content">
+    <meta name="robots" content="INDEX, FOLLOW">
     <meta name="author" content="Author">
+    <meta name="geo.placename" content="City/State (for instance, 'Los Angeles')">
+    <meta name="geo.region" content="Country (for instance, 'US')">
+
+    <link rel="canonical" href="[WEB ADDRESS]">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <meta property="og:title" content="Website Title.">
+  <!-- Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Website Name">
+    <meta property="og:title" content="Website Title">
     <meta property="og:description" content="Website description.">
-    <meta property="og:image" content="[WEB ADDRESS]/images/anyimage.png">
+    <meta property="og:image" content="[WEB ADDRESS]/images/social.png">
     <meta property="og:url" content="[WEB ADDRESS]">
+    <meta property="fb:app_id" content="[#ID]">
+
+  <!-- Twitter -->
+    <meta property="twitter:card" content="summary">
+    <meta property="twitter:site" content="[@USERNAME]">
+    <meta property="twitter:title" content="Website Title">
+    <meta property="twitter:description" content="Website description.">
+    <meta property="twitter:image" content="[WEB ADDRESS]/images/social.png">
+    <meta property="twitter:url" content="[WEB ADDRESS]">
 
   <!-- CSS & Fonts -->
     <link type="text/css" rel="stylesheet" href="css/dist/style.css">
-    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:100,300,500">
-
-  <!--[if lt IE 9]>
-    <script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
+    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
 
   <!-- Favicon & iPhone Icon -->
     <link rel="icon" type="image/png" href="[WEB ADDRESS]/images/favicon.png">
-    <link rel="shortcut icon" href="[WEB ADDRESS]/images/favicon.png">
+    <link rel="shortcut icon" type="image/png" href="[WEB ADDRESS]/images/favicon.png">
     <link rel="apple-touch-icon-precomposed" sizes="60x60" href="[WEB ADDRESS]/images/apple-touch-icon-iphone.png">
     <link rel="apple-touch-icon-precomposed" sizes="76x76" href="[WEB ADDRESS]/images/apple-touch-icon-ipad.png">
     <link rel="apple-touch-icon-precomposed" sizes="120x120" href="[WEB ADDRESS]/images/apple-touch-icon-iphone@2x.png">
     <link rel="apple-touch-icon-precomposed" sizes="152x152" href="[WEB ADDRESS]/images/apple-touch-icon-ipad@2x.png">
 
-  <title>Website Title</title>
+  <!--[if lt IE 9]>
+    <script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Others -->
+    <meta name="X-UA-Compatible" content="IE=edge">
+    <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE">
+    <meta name="apple-mobile-web-app-capable" content="no">
 </head>
 
 <body>
